50aaf8443da9e5376851f2b23cf0a25d7490781d
Justin <justinna777@gmail.com>
2024-12-06 14:24:25 -0800

    Change to product apis

--- server/app.py
+++ server/app.py
@@ -14,10 +14,10 @@ from game_system.accusation import Accusation
 from game_system.BoardManager import BoardManager
 
 app = Flask(__name__)
-# CORS(app, origins=["https://peppy-empanada-ec068d.netlify.app"])
-# socketio = SocketIO(app, cors_allowed_origins="https://peppy-empanada-ec068d.netlify.app")
-CORS(app, origins=["http://192.168.1.22:3001"])
-socketio = SocketIO(app, cors_allowed_origins="http://192.168.1.22:3001")
+CORS(app, origins=["https://peppy-empanada-ec068d.netlify.app"])
+socketio = SocketIO(app, cors_allowed_origins="https://peppy-empanada-ec068d.netlify.app")
+# CORS(app, origins=["http://192.168.1.22:3001"])
+# socketio = SocketIO(app, cors_allowed_origins="http://192.168.1.22:3001")
 # CORS(app, origin=["http://localhost:3000"])
 # socketio = SocketIO(app, cors_allowed_origins="http://localhost:3000")
 
